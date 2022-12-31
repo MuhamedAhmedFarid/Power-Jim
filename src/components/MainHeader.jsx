e65c5ec076a99725effc9fb16d0e77f9fb233199
muhamd <muhamedahmed9099@gmail.com>
2022-12-31 17:18:19 +0200

    done

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Image from "../images/main_header.png";
-import Zoom from 'react-reveal/Zoom';
+import Zoom from "react-reveal/Zoom";
 
 function MianHeader() {
   return (
@@ -11,21 +11,22 @@ function MianHeader() {
           <h4>#100DaysOFWorkUt</h4>
           <h1>Join The Legends Of The Fitness World</h1>
           <p>
-            lorem lorem lorem lorem loreml lo' lorem lorem lorem lorem lorem
-            lorem
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, in sint
+            consequatur itaque voluptas aliquid cupiditate minima necessitatibus
+            .
           </p>
           <Link className="btn lg" to="/plans">
             Get Started
           </Link>
         </div>
         <div className="main__header-right">
-            <Zoom bottom>
-                <div className="main__header-circle">
-                    <div className="main__header-img">
-                    <img src={Image} />
-                    </div>
-                </div>
-            </Zoom>
+          <Zoom bottom>
+            <div className="main__header-circle">
+              <div className="main__header-img">
+                <img src={Image} />
+              </div>
+            </div>
+          </Zoom>
         </div>
       </div>
     </header>
--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import Header from '../../components/Header'
-import HeaderImg from '../../images/header_bg_5.jpg'
-import {traniners} from '../../data'
-import { FaLinkedin } from 'react-icons/fa'
-import { FaFacebook } from 'react-icons/fa'
-import { AiOutlineTwitter } from 'react-icons/ai'
-import { AiFillInstagram } from 'react-icons/ai'
-import './trainers.css'
+import React from "react";
+import Header from "../../components/Header";
+import HeaderImg from "../../images/header_bg_5.jpg";
+import { trainers } from "../../data";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import Trainer from "../../components/Trainer";
+import Fade from 'react-reveal/Fade';
+import "./trainers.css";
 function Trainers() {
   return (
     <>
-      <Header img={HeaderImg} title='Our Trainers' children=
-      'Facilis, iusto numquam nude laborriosam expedita qui exercitation? Dictavero accusantium est aaut meletiae fugit dolormque suscipit quod.'
+      <Header
+        img={HeaderImg}
+        title="Our Trainers"
+        children="Facilis, iusto numquam nude laborriosam expedita qui exercitation? Dictavero accusantium est aaut meletiae fugit dolormque suscipit quod."
       />
-    </>
+      <section className="trainers">
+        <div className="container trainers__container">
+          {trainers.map(({ id, image, name, job, socials }) => {
+            return (
+              <Fade bottom>
 
-  )
+              
+              <Trainer
+                image={image}
+                name={name}
+                job={job}
+                key={id}
+                socials={[
+                  { icon: <AiFillInstagram />, link: socials[0] },
+                  { icon: <AiOutlineTwitter />, link: socials[1] },
+                  { icon: <FaFacebook />, link: socials[2] },
+                  { icon: <FaLinkedin />, link: socials[3] },
+                ]}
+              />
+            </Fade>  
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Trainers
+export default Trainers;
